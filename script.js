44a67ca16818d8c71db90efc31127942b26155ef
Forrest Nelson <forrestnnelson@gmail.com>
2023-07-15 22:33:33 -0500

    updated script.js, working for adding, not yet working for subtracting. still working on getting US dollar format for added annual salaries

--- script.js
+++ script.js
@@ -14,6 +14,8 @@ function onReady() {
 
 }
 
+let monthlySalaryTotal = 0;
+
 function handleSubmit(event) {
     event.preventDefault(); // telling jQuery to stop the default event of clicking a submit button for a form
     console.log("inside handleSubmit")
@@ -23,6 +25,14 @@ function handleSubmit(event) {
     const idNumber = $('#id-number').val()
     const jobTitle = $('#job-title').val()
     const annualSalary = $('#annual-salary').val()
+    console.log("added annual salary:", annualSalary)
+
+    // adding new annualSalary to global variable monthlySalaryTotal
+    monthlySalaryTotal += annualSalary / 12;
+    if (monthlySalaryTotal > 20000) {
+        $('#total-monthly-display').addClass('red-background');
+    }
+    $('#total-monthly-display').text(`Total Monthly: ${monthlySalaryTotal.toLocaleString("en-US", { style: "currency", currency: "USD" })}`)
 
     // add employee info into table upon click Submit
     $('#table-employees').append(`
@@ -31,22 +41,40 @@ function handleSubmit(event) {
         <td>${lastNameEmployee}</td>
         <td>${idNumber}</td>
         <td>${jobTitle}</td>
-        <td class="annual-salary-cell">$${annualSalary}</td >
+        <td id="annual-salary-cell">$${annualSalary}</td>
         <td><button id="delete-button">Delete</button></td>
-    </tr >
+    </tr>
         `)
     // ^ this adds a table row of table data, using the values of newly declared variables (which themselves get the values we wrote in the inputs using a getter .val())
     // I also found out you can just add $ in front of the ${annualSalary} which might make it easier to use the calc?
     // made sure to add class and id for salary/delete button to use with handlers and calculations
+    // For the life of me cannot understand why we can't add ${annualSalary.toLocaleString("en-US", { style: "currency", currency: "USD" })
+    $('#annual-salary-cell').text()
 
     $("#first-name").val("");
     $('#last-name').val("");
     $('#id-number').val("");
     $('#job-title').val("");
     $('#annual-salary').val("");
+    // used the setter form of .val(value) to clear the text boxes
 }
 
 function handleDelete() {
+    // attempt:
+    // define variable as value of same row's annual salary cell
+    // how to do this?  tried using $(this).val() but that'd only get the button value.  $(this).parent.(#annual-salary-cell)
+
+    const specificEmployeeSalary = $(this).parent().siblings('#annual-salary-cell').val()
+    console.log(specificEmployeeSalary);
+    // monthlySalaryTotal -= specificEmployeeSalary / 12;
+    // $('#total-monthly-display').text(monthlySalaryTotal)
+
+    if (monthlySalaryTotal < 20000) {
+        $('#total-monthly-display').removeClass('red-background');
+    }
+
     $(this).parent().parent().remove();
     // this refers to what we're clicking on, first parent is <td>, second parent is <tr>, and we're removing this <tr>
+
 }
+
